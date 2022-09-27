5d802ee432b88d1e6dd0392d9c39bdac6c8bbeaa
hanguang254 <725204548@qq.com>
2022-09-27 15:49:03 +0800

    renew

--- TestCase/Run.py
+++ TestCase/Run.py
@@ -4,8 +4,10 @@
 @Author ：le
 @Date ：2022/8/25 14:23 
 '''
+import os
 import pytest
+
 
 if __name__ == '__main__':
     pytest.main()
-
+    os.system("allure generate ../test_Report/json –o ../test_Report/report_test --clean")
